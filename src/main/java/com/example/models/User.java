d23db69b42b2852e4ee9ea6382ef321948256ed9
Tahircan45 <tahircanakgun@gmail.com>
2021-01-06 23:06:43 +0300

    Revert "Revert "user manager interface added""
    This reverts commit 33aa307e74c64297710bb33c57f8b2396b8303b7.

--- src/main/java/com/example/models/User.java
+++ src/main/java/com/example/models/User.java
@@ -23,7 +23,7 @@ public class User {
 
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @ManyToMany(cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name="user_id"),
             inverseJoinColumns = @JoinColumn(name="role_id"))
